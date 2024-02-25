59819e7219293e0bd65cbecfd2868f01d4c6dce1
eduardoudg <1510.lalo.51@gmail.com>
2024-02-24 22:55:13 -0600

    update - command to build updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  BASE_API: 'http://localhost:8080/MiRutina/api',
+  // BASE_API: 'http://localhost:8080/MiRutina/api',
+  BASE_API: (window.location.protocol === 'http:'
+              ? 'http://'
+              : 'https://')
+              + window.location.host + '/MiRutina/api',
 };
