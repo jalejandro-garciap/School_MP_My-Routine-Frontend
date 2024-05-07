d11eaa7ba9f425cac895bfc8060e72dec07b8e37
eduardo_dorado <eduardo.dorado@sanmina.com>
2024-05-07 13:32:15 -0600

    update exercise chart added

--- src/app/dashboard/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/dashboard/pages/dashboard-page/dashboard-page.component.ts
@@ -1,83 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+
+import { HistoriesService } from 'src/app/histories/services/histories.service';
 import { ExerciseRecord } from 'src/app/users/interfaces/exercise-record.interface';
-import { ExerciseRecordService } from 'src/app/users/services/exercise-record.service';
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.scss']
 })
-export class DashboardPageComponent {
+export class DashboardPageComponent implements OnInit {
 
-  // public labels1: string[] = [];
-  // public data1: number[] = [];
+  public _labels1:  string[]  = [];
+  public _data1:    number[]  = [];
+
+  public _labels3:  string[]  = [];
+  public _data3:    number[]  = [];
 
   public completedChallenges: ExerciseRecord[] = [];
 
   constructor(
-    private _exerciseRecord: ExerciseRecordService
-  ) {
-    
-    // this._exerciseRecord.getAll().subscribe({
-    //   next: response => {
-      
-    //     // const CHILDREN = response.data.filter( exercise => exercise.passenger.passengerType === 'CHILDREN' ).length;
-    //     // const STUDENTS = response.data.filter( exercise => exercise.passenger.passengerType === 'STUDENTS' ).length;
-    //     // const TEACHERS = response.data.filter( exercise => exercise.passenger.passengerType === 'TEACHERS' ).length;
-    //     // const DISABLED = response.data.filter( exercise => exercise.passenger.passengerType === 'DISABLED' ).length;
-    //     // const ELDERLY = response.data.filter( exercise => exercise.passenger.passengerType === 'ELDERLY' ).length;        
+    private _historiesService:HistoriesService    
+  ) {}
 
-    //     // const sort = [CHILDREN, STUDENTS, TEACHERS, DISABLED, ELDERLY].sort(function(a, b) {
-    //     //   return b - a;
-    //     // });
-
-
-    //     // const CHILDREN_VALUE = CHILDREN;
-    //     // const STUDENTS_VALUE = STUDENTS;
-    //     // const TEACHERS_VALUE = TEACHERS;
-    //     // const DISABLED_VALUE = DISABLED;
-    //     // const ELDERLY_VALUE = ELDERLY;
-
-
-    //     const newLabels1: string[] = [];
-    //     // const top3 = sort.slice(0, 3);
-
-    //     // top3.forEach( top => {
-    //     //   if(top === CHILDREN_VALUE) newLabels1.push('CHILDREN');
-    //     //   if(top === STUDENTS_VALUE) newLabels1.push('STUDENTS');
-    //     //   if(top === TEACHERS_VALUE) newLabels1.push('TEACHERS');
-    //     //   if(top === DISABLED_VALUE) newLabels1.push('DISABLED');
-    //     //   if(top === ELDERLY_VALUE) newLabels1.push('ELDERLY');
-    //     // });
-
-    //     this.labels1 = [...newLabels1];
-    //     // this.data1 = [...top3];
-    //   }
-    // });
-
-
-    // this.data2 = [18, 30, 9];
-
+  ngOnInit(): void {
+    this.loadHistories();
   }
 
-  // ngOnInit(): void {
-  //   this.getCompletedChallengeToday();
-  // }
+  loadHistories():void {
+    this._historiesService.getHistories().subscribe({
+      next: (response) => {
 
-  // getCompletedChallengeToday():void {
-  //   this._exerciseRecord.getAllWhoCompletedChallengeToday().subscribe({
-  //     next: completedChallenges => {
-  //       this.completedChallenges = completedChallenges;
-  //     }
-  //   });
-  // }
+        const exerciseFilter = response.data.map( history => history.exercise);
+
+        const exerciseFilterLabel = ['Squats', 'Push ups'];
+        const squatsCount: number   = exerciseFilter.filter( exercise => exercise === 'Squats').length;
+        const pushUpsCount: number  = exerciseFilter.filter( exercise => exercise === 'Push ups').length;
+        this._labels3 = [...exerciseFilterLabel];
+        this._data3   = [ squatsCount, pushUpsCount ];
+
+        console.log({ exerciseFilter });
+      }
+    });
+  }
 
   get labels1() {
-    return ['Admin', 'Elderly', 'Teacher', 'Children', 'Student'];
+    return this._labels1;
   }
 
   get data1() {
-    return [6,5,7,6,13];
+    return this._data1;
   }
 
   get labels2() {
@@ -86,6 +57,14 @@ export class DashboardPageComponent {
 
   get data2() {
     return [1,3,11,1,1,2,1];
+  }
+
+  get labels3() {
+    return this._labels3;
+  }
+
+  get data3() {
+    return this._data3;
   }
 
 }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  BASE_API: 'http://localhost:8080/api',
+  BASE_API: 'https://my-routine.up.railway.app/api',
 };
